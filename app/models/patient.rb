f926e1f9c2303aaf067c4729d7394b4d38cee170
tbenny <tony.benny@coxautoinc.com>
2021-08-07 21:46:08 -0400

    Adding basic validations to patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,6 @@
 class Patient < ApplicationRecord
   belongs_to :state
+
+  validates_format_of :full_name, with: /[A-Za-z] [A-Za-z]\z/
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
